27ac99d88c4367e3d71691d89a22e5deb74e0838
matiasjgelpi <matiasjgelpi@gmail.com>
2022-07-28 15:39:51 -0300

    Update index.js

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ app.get('/iecho', reverseText)
 
 app.use('/', noEndpointReached)
 
-const server = app.listen(4000, () => {
-  console.log('Server is running on port 4000')
+const server = app.listen(8080, () => {
+  console.log('Server is running on port 8080')
 })
 
 module.exports = { app, server }
